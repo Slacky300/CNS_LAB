4c98ba092f20a2e77bc86976bbb15996c2c18d3b
Slacky300 <rehman.khan.300a@gmail.com>
2023-11-05 19:33:10 +0530

    update

---
+++ Exp4/AES.py
@@ -0,0 +1,27 @@
+import base64
+import hashlib
+from Crypto.Cipher import AES
+from Crypto import Random
+BLOCK_SIZE = 16
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
+unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+def encrypt(plain_text, key):
+    private_key = hashlib.sha256(key.encode("utf-8")).digest()
+    plain_text = pad(plain_text)
+    print("After padding:", plain_text)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(private_key, AES.MODE_CBC, iv)
+    return base64.b64encode(iv + cipher.encrypt(plain_text))
+def decrypt(cipher_text, key):
+    private_key = hashlib.sha256(key.encode("utf-8")).digest()
+    cipher_text = base64.b64decode(cipher_text)
+    iv = cipher_text[:16]
+    cipher = AES.new(private_key, AES.MODE_CBC, iv)
+    return unpad(cipher.decrypt(cipher_text[16:]))
+message=input("Enter message to encrypt: ");
+key = input("Enter encryption key: ")
+encrypted_msg = encrypt(message, key)
+print("Encrypted Message:", encrypted_msg)
+decrypted_msg = decrypt(encrypted_msg, key)
+print("Decrypted Message:", bytes.decode(decrypted_msg))
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Exp 4 does not work 
+
+# NOTE:
+ if you encounter any error in any of the following codes please raise an issue
